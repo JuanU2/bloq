7f7f8da11b3ec25a9e1a5af66702196e3c229640
Juan Marcos <juanmarcosu2@gmail.com>
2025-09-10 18:31:07 +0200

    add postlist

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { siteTitle } from '$lib/config';
+	import PostsList from '$lib/components/PostsList.svelte';
+import { siteTitle } from '$lib/config';
 
 	let { data } = $props();
 </script>
@@ -9,4 +10,12 @@
 </svelte:head>
 
 <!-- This is the README.md file in the root of the repo. It serves double duty as the homepage's content. If you'd rather use your own HTML and/or Svelte, you can delete/modify everything in this file. -->
-<data.ReadMe />
+<h1>
+		Vitajte !
+</h1>
+
+<p>
+	Tento blog som začal s účelom písať o témach ktoré ma zaujímajú, rád by som rozšíril povedomie o tom, ako fungujú bežné nástroje, ktoré denno denne používame <i>( ako napríklad internet )</i> pod kapotou.
+</p>
+<hr>
+<PostsList posts={data.posts}/>
